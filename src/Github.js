999e2002527ea95c5af69e7e303445508b6c1b16
Lucas Isla <lisla@purdue.edu>
2017-06-20 11:49:06 -0400

    Add route for exact match of '/github'

--- src/Github.js
+++ src/Github.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import './Github.css'
 
 class Github extends Component{
+
+  state = {
+    username: '',
+  }
+
+  handleChange = (ev) => {
+    const username = ev.currentTarget.value
+    this.setState({ username })
+  }
+
+
   render(){
     return(
       <div className="github">
-        <img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="Github Logo"/>
+        <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="Github Logo"/>
         <form>
           <div>
-            <input type="text"/>
+            <input 
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <button type="submit">Look up Github user</button>
           </div>
         </form>
+        <Route exact path='/github' render={() => <h3>Please enter a username to search on Github</h3>} />
       </div>
     )
   }
